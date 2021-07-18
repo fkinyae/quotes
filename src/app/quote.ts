c8287b246a22c0b308ecde3148f5981ccad50e43
Francis Kinyae <fmkinyae@gmail.com>
2021-07-18 19:18:31 +0300

    add a new date property

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     
-    constructor(public id:number, public quote:string, public description: string, public author: string, public submit_by:string  ) {
+    constructor(public id:number, public quote:string, public description: string, public author: string, public submit_by:string , public postDate: Date ) {
 
     }
 }
