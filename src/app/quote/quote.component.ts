52e4cbe936105df532eacb437f4d5e55ef4ae102
Francis Kinyae <fmkinyae@gmail.com>
2021-07-18 19:24:24 +0300

    add class for the date property

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,17 +9,17 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes: Quote[] = [
-    new Quote(1,  'Lions and Hunting', 'Until the lions have their own historians, the history of the hunt will always glorify the hunter', 'Chinua Achebe', 'Armando Broja'  ),
+    new Quote(1,  'Lions and Hunting', 'Until the lions have their own historians, the history of the hunt will always glorify the hunter', 'Chinua Achebe', 'Armando Broja' , new Date(2020,6,12) ),
 
-    new Quote (2, 'Humans and Reality',  'Its funny how humans can wrap their mind around things and fit them into their version of reality.', 'Rick Riordan, The Lightning Thief', 'Spinazzola Fozza' ),
+    new Quote (2, 'Humans and Reality',  'Its funny how humans can wrap their mind around things and fit them into their version of reality.', 'Rick Riordan', 'Spinazzola Fozza' , new Date(2021,3,10)),
 
-    new Quote(3, 'Love And Compassion', 'Love and compassion are necessities, not luxuries. Without them, humanity cannot survive.', ' Dalai Lama XIV, The Art of Happiness', 'Dani Chalobah'),
+    new Quote(3, 'Love And Compassion', 'Love and compassion are necessities, not luxuries. Without them, humanity cannot survive.', ' Dalai Lama XIV', 'Dani Chalobah', new Date(2021,4,5)),
 
-    new Quote(4, 'Man And Anxieties', 'Man is not worried by real problems so much as by his imagined anxieties about real problems', 'Epictetus', 'Billy Gilmour'),
+    new Quote(4, 'Man And Anxieties', 'Man is not worried by real problems so much as by his imagined anxieties about real problems', 'Epictetus', 'Billy Gilmour', new Date(2021,9,11)),
 
-    new Quote(5, 'History And Future', 'History should not be just a light to the past but a signal light to the future.', 'Sijin Bt', 'Danny Musonda'),
+    new Quote(5, 'History And Future', 'History should not be just a light to the past but a signal light to the future.', 'Sijin Bt', 'Danny Musonda', new Date(2021,7,3)),
 
-  new Quote(5, 'Man And Inhumanity', 'I am constantly amazed by mans inhumanity to man.', 'Primo Levi, If This Is a Man The Truce', 'Tino Livramento'),
+  new Quote(5, 'Man And Inhumanity', 'I am constantly amazed by mans inhumanity to man.', 'Primo Levi', 'Tino Livramento',new Date(2020,8,2)),
   ];
 
   addNewQuote(quote:any) {
